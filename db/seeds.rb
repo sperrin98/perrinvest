02394084c90d31714116974f2c65ddbe3d714fe6
stanleyperrin <stanperrin98@gmail.com>
2022-07-21 13:12:02 +0100

    fonts

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require 'csv'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +6,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+gold = Stock.create(name: "Gold")
+puts "Stock: Gold created"
+
+filepath = '/Users/stanleyperrin/code/sperrin98/perrinvest/gold.csv'
+
+CSV.foreach(filepath, headers: true) do |row|
+  row = History.create({
+    :stock_id  => row[0],
+    :date => row[1],
+    :price => row[2]
+  })
+end
