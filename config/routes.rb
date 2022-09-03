40c5e6d227da4e08589fdff6485f118f1c9ce2c0
stanleyperrin <stanperrin98@gmail.com>
2022-09-03 18:24:58 +0100

    fixed signout

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
   get 'home/index'
-  devise_for :users
+  devise_for :users do
+  get '/users/sign_out' => 'devise/sessions#destroy'
+end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "home#index"
   get "/about", to: "pages#home"
 
-  # resources :stocks do
-  #   resources :histories, only: [:index, :show ]
-  # end
+  resources :stocks do
+    resources :histories, only: [:index, :show ]
+  end
 end
