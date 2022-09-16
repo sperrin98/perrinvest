df5df9a8d822aaa78579d8a0515af97c12d280c4
stanleyperrin <stanperrin98@gmail.com>
2022-09-16 14:56:57 +0100

    save review

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,5 +1,10 @@
 class BlogsController < ApplicationController
   def index
+    @blogs = Blog.all
+  end
+
+  def show
+    @blog = Blog.new
   end
 
   def new
@@ -7,5 +12,17 @@ class BlogsController < ApplicationController
   end
 
   def create
+    @blog = Blog.new(blog_params)
+    if @blog.save
+      redirect_to blogs_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def blog_params
+    params.require(:blog).permit(:title, :content, :publisher, :published_at)
   end
 end
