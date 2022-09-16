34d1cc48fb2b77ab63a7ea40fda5bf9dd2765dba
stanleyperrin <stanperrin98@gmail.com>
2022-09-16 17:58:48 +0100

    fixed cards

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,4 +1,6 @@
 class Blog < ApplicationRecord
 
   validates :title, :subtitle, :content, :publisher, presence: true
+  validates :subtitle, length: { minimum: 50 }
+  validates :content, length: { in: 250..5000 }
 end
