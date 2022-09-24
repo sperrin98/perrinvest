ffa15b34c3657c0e07ba050df19fb67099369853
stanleyperrin <stanperrin98@gmail.com>
2022-09-24 16:13:10 +0100

    images

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -24,6 +24,6 @@ class BlogsController < ApplicationController
   private
 
   def blog_params
-    params.require(:blog).permit(:id, :title, :content, :publisher, :published_at, :subtitle)
+    params.require(:blog).permit(:id, :title, :content, :publisher, :published_at, :subtitle, :image)
   end
 end
