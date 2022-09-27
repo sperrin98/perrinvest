32a75d94aa3b5e53f1c16b910562ab846227bf28
stanleyperrin <stanperrin98@gmail.com>
2022-09-27 09:45:58 +0100

    validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :authentication_keys => [:username]
+
+  validates :email, uniqueness: true
+  validates :username, uniqueness: true
 end
