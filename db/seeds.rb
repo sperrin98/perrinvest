ae2fa2b3a00458e6d55efcdd45277db8aada4bb4
stanleyperrin <stanperrin98@gmail.com>
2022-09-28 16:49:17 +0100

    cad v usd

--- db/seeds.rb
+++ db/seeds.rb
@@ -314,3 +314,17 @@ CSV.foreach(Rails.root.join(jpyvusd_filepath), headers: true) do |x|
   puts "#{x[0]}, #{x[1]}, #{x[2]}, #{x[3]}"
 end
 puts "JPY v USD histories created"
+
+cadvusd_filepath = '/Users/stanleyperrin/code/sperrin98/perrinvest/cadvusd.csv'
+
+CSV.foreach(Rails.root.join(cadvusd_filepath), headers: true) do |x|
+  History.create!( {
+    id: x["id"],
+    stock_id: x["stock_id"],
+    date: x["date"],
+    price: x["price"]
+  })
+
+  puts "#{x[0]}, #{x[1]}, #{x[2]}, #{x[3]}"
+end
+puts "CAD v USD histories created"
