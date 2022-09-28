d3eec3c9a2915371b289c6853cefd2484dd17c56
stanleyperrin <stanperrin98@gmail.com>
2022-09-28 18:16:28 +0100

    controller

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,18 @@
+class CommentsController < ApplicationController
+
+  def new
+    @comment = Comment.new
+  end
+
+  def create
+    @blog = Blog.find(params[:blog_id])
+    @comment = @blog.comments.create(comment_params)
+    redirect_to blog_path(@blog)
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:comment)
+  end
+end
