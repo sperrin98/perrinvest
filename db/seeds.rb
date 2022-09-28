0e0c32f5530edf0e8118d8752efa804ae2d930b3
stanleyperrin <stanperrin98@gmail.com>
2022-09-28 16:31:35 +0100

    gbp v usd

--- db/seeds.rb
+++ db/seeds.rb
@@ -272,3 +272,17 @@ CSV.foreach(Rails.root.join(amazon_filepath), headers: true) do |x|
   puts "#{x[0]}, #{x[1]}, #{x[2]}, #{x[3]}"
 end
 puts "Amazon histories created"
+
+gbpvusd_filepath = '/Users/stanleyperrin/code/sperrin98/perrinvest/gbpvusd.csv'
+
+CSV.foreach(Rails.root.join(gbpvusd_filepath), headers: true) do |x|
+  History.create!( {
+    id: x["id"],
+    stock_id: x["stock_id"],
+    date: x["date"],
+    price: x["price"]
+  })
+
+  puts "#{x[0]}, #{x[1]}, #{x[2]}, #{x[3]}"
+end
+puts "GBP v USD histories created"
