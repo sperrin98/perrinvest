02f0eedaca12ecea6b68f5cbb1d3e8401b4846d0
stanleyperrin <stanperrin98@gmail.com>
2022-10-11 14:00:25 +0100

    blog validations

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,4 +1,9 @@
 class Blog < ApplicationRecord
   belongs_to :user
+  has_one_attached :image_url
   has_many :comments, dependent: :destroy
+
+  validates :title, :subtitle, :content, presence: true
+  validates :subtitle, length: { in: 15..500 }
+  validates :content, length: { in: 250..5000 }
 end
