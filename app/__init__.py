074a4794c138d35ca20c2a64991484611010e289
sperrin98 <stanperrin98@gmail.com>
2024-06-11 13:24:50 +0100

    Initial commit

---
+++ app/__init__.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object('config.Config')
+
+    db.init_app(app)
+
+    from app.main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    return app
