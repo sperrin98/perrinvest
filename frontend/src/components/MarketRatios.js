8fcde46040d335f9aef5f98d619cc18d985ede21
sperrin98 <stanperrin98@gmail.com>
2024-06-12 16:35:25 +0100

    price-histories-and-market-ratios

--- frontend/src/components/MarketRatios.js
+++ frontend/src/components/MarketRatios.js
@@ -1,7 +1,6 @@
-// frontend/src/components/MarketRatios.js
-
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { Link } from 'react-router-dom'; // Import Link from react-router-dom
 
 function MarketRatios() {
   const [marketRatios, setMarketRatios] = useState([]);
@@ -18,14 +17,25 @@ function MarketRatios() {
 
   return (
     <div>
-      <h1>Market Ratios</h1>
-      <ul>
-        {marketRatios.map(ratio => (
-          <li key={ratio.id}>
-            {ratio[0]}: {ratio[1]}
-          </li>
-        ))}
-      </ul>
+      <h1>Market Ratios Data</h1>
+      <Link to="/">Go Back to Homepage</Link>
+      <table>
+        <thead>
+          <tr>
+            <th>Ratio ID</th>
+            <th>Ratio Name</th>
+          </tr>
+        </thead>
+        <tbody>
+          {marketRatios.map(ratio => (
+            <tr key={ratio.ratio_id}>
+              <td>{ratio[0]}</td>
+              <td>{ratio[1]}</td>
+              <a href={`/market-ratios/${ratio[0]}`}>View Details</a>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
