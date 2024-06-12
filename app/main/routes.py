3a0bbb26b166095a8979c9d52e6acafb3a9d4931
sperrin98 <stanperrin98@gmail.com>
2024-06-12 13:30:04 +0100

    react-server

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,33 +1,50 @@
-from flask import render_template
-from app.main import main
+from . import main
+from flask import jsonify, Flask
+from flask_cors import CORS
 from app.db_utils import fetch_securities, fetch_market_ratios, fetch_market_ratio_data, fetch_security_data, fetch_price_history
+
+app = Flask(__name__)
+CORS(app)
 
 @main.route('/')
 def home():
-    return render_template('home.html')
+    return jsonify({"message": "Welcome to Perrinvest"})
 
 @main.route('/securities')
-def securities():
+def get_securities():
     securities = fetch_securities()
     print(securities)  # Add this line to check the data
-    return render_template('securities.html', securities=securities)
+    return jsonify(securities)
 
 @main.route('/securities/<int:security_id>')
-def security_detail(security_id):
+def security(security_id):  # Updated function name
+    # Fetch security data
     security = fetch_security_data(security_id)
     price_history = fetch_price_history(security_id)
-    return render_template('security.html', security=security, price_history=price_history)
+    # Construct JSON response
+    response = {
+        "security": security,
+        "price_history": price_history
+    }
+    # Return JSON response
+    return jsonify(response)
 
 @main.route('/market-ratios')
 def market_ratios():
+    # Fetch market ratios data
     market_ratios_data = fetch_market_ratios()
-    print(market_ratios_data)
-    return render_template('market_ratios.html', market_ratios=market_ratios_data)
+    # Return JSON response
+    return jsonify(market_ratios_data)
 
-@main.route('/market-ratios/<int:market_ratio_id>')
 def market_ratio(market_ratio_id):
+    # Fetch market ratio data
     market_ratio_data = fetch_market_ratio_data(market_ratio_id)
     ratio_name = market_ratio_data[0][0] if market_ratio_data else "Unknown Ratio"
     market_ratio_data = [(row[1], row[2]) for row in market_ratio_data]  # Remove the ratio name from each row
-    return render_template('market_ratio.html', market_ratio=market_ratio_data, ratio_name=ratio_name)
-
+    # Construct JSON response
+    response = {
+        "ratio_name": ratio_name,
+        "market_ratio": market_ratio_data
+    }
+    # Return JSON response
+    return jsonify(response)
