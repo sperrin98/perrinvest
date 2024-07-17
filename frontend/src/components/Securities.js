200c38b2b586ba89365b80f092f58ca1fc3c6a24
sperrin98 <stanperrin98@gmail.com>
2024-07-17 16:45:54 +0100

    'securities-frontend'

--- frontend/src/components/Securities.js
+++ frontend/src/components/Securities.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { Link } from 'react-router-dom'; // Import Link from react-router-dom
+import { Link, useNavigate } from 'react-router-dom';
+import './Securities.css'; // Import the CSS file
 
 function Securities() {
   const [securities, setSecurities] = useState([]);
+  const navigate = useNavigate();
 
   useEffect(() => {
     axios.get('http://localhost:5000/securities')
@@ -15,33 +17,34 @@ function Securities() {
       });
   }, []);
 
+  const handleRowClick = (id) => {
+    navigate(`/securities/${id}`);
+  };
+
   return (
-    <div>
-      <h1>Securities Data</h1>
-      <Link to="/">Go Back to Homepage</Link>
-      <div>
-      </div>
+    <div className="securities-container">
+      <h1 className="security-header">Securities Data</h1>
       <table>
         <thead>
           <tr>
-            <th>Security ID</th>
-            <th>Security Long Name</th>
+            <th className='id-header'></th>
+            <th className="first-header">Security Long Name</th>
             <th>Security Short Name</th>
           </tr>
         </thead>
         <tbody>
           {securities.map(security => (
-            <tr key={security.security_id}>
+            <tr key={security.security_id} onClick={() => handleRowClick(security[0])} className="clickable-row">
               <td>{security[0]}</td>
-              <td>{security[1]}</td>
+              <td className="first-column">{security[1]}</td>
               <td>{security[2]}</td>
-              <td>
-              <a href={`/securities/${security[0]}`}>View Details</a>
-              </td>
             </tr>
           ))}
         </tbody>
       </table>
+      <div className="back-button">
+        <Link to="/">Go Back to Homepage</Link>
+      </div>
     </div>
   );
 }
