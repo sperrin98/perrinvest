a7f68e7c2e36f3027ef2186805de65a44257405d
sperrin98 <stanperrin98@gmail.com>
2024-07-18 20:16:47 +0100

    'candlestick-bg'

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,7 +1,5 @@
-import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
 import './Home.css'; // Import the CSS file
-import parallaxImage from '../assets/images/parallax-image.png'; // Correct path
 
 const Home = () => {
   const [displayedText, setDisplayedText] = useState('');
@@ -27,36 +25,12 @@ const Home = () => {
   }, [text]);
 
   return (
-    <div className="home-container">
-      <div className="parallax-background"></div>
-      <div className="content-container">
+    <div>
+      <div className="section1">
         <h1 className="home-header">{displayedText}</h1>
-        <div className="home-buttons">
-          <Link to="/securities">
-            <div className="center">
-              <button className="btn">
-                <svg width="180px" height="60px" viewBox="0 0 180 60" className="border">
-                  <polyline points="179,1 179,59 1,59 1,1 179,1" className="bg-line" />
-                  <polyline points="179,1 179,59 1,59 1,1 179,1" className="hl-line" />
-                </svg>
-                <span>Securities</span>
-              </button>
-            </div>
-          </Link>
-        </div>
-        <div className="home-buttons">
-          <Link to="/market-ratios">
-            <div className="center">
-              <button className="btn">
-                <svg width="180px" height="60px" viewBox="0 0 180 60" className="border">
-                  <polyline points="179,1 179,59 1,59 1,1 179,1" className="bg-line" />
-                  <polyline points="179,1 179,59 1,59 1,1 179,1" className="hl-line" />
-                </svg>
-                <span>Market Ratios</span>
-              </button>
-            </div>
-          </Link>
-        </div>
+      </div>
+      <div className="section2">
+        <h1 className="home-header">Securities</h1>
       </div>
     </div>
   );
