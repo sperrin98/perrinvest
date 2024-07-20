4e693025ef5bef30cc32abb00c1117f514b36345
sperrin98 <stanperrin98@gmail.com>
2024-07-20 19:07:32 +0100

    frontend

--- frontend/src/components/ApplePriceChart.js
+++ frontend/src/components/ApplePriceChart.js
@@ -42,8 +42,8 @@ const ApplePriceChart = () => {
   }, []);
 
   return (
-    <div style={{ width: '75%', height: '75%'}}>
-    <h2>Apple Price History</h2>
+    <div style={{ width: '50%', height: '50%'}}>
+    <h2 className='carousel-header'>Apple Price History</h2>
       <Chart
         type='candlestick'
         data={chartData}
--- frontend/src/components/SP500PriceChart.js
+++ frontend/src/components/SP500PriceChart.js
@@ -42,8 +42,8 @@ const SP500PriceChart = () => {
   }, []);
 
   return (
-    <div style={{ width: '75%', height: '75%'}}>
-    <h2>S&P500 Price History</h2>
+    <div style={{ width: '50%', height: '50%'}}>
+    <h2 className='carousel-header'>S&P500 Price History</h2>
       <Chart
         type='candlestick'
         data={chartData}
