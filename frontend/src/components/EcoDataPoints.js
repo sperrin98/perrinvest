ffe2a6bf891011d645fb8ea8cf18b0e69c9d5058
sperrin98 <stanperrin98@gmail.com>
2024-07-22 23:02:40 +0100

    eco-data-points-histories

--- frontend/src/components/EcoDataPoints.js
+++ frontend/src/components/EcoDataPoints.js
@@ -1,24 +1,23 @@
-// src/EcoDataPointsPage.js
+// components/EcoDataPoints.js
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import './EcoDataPoints.css'
 
-function EcoDataPointsPage() {
-  const [data, setData] = useState([]);
+function EcoDataPoints() {
+  const [ecoDataPoints, setEcoDataPoints] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
   useEffect(() => {
     fetch('http://localhost:5000/eco-data-points')
-      .then(response => {
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        return response.json();
-      })
+      .then(response => response.json())
       .then(data => {
-        setData(data);
+        console.log(data);  // Debugging: log data to console
+        setEcoDataPoints(data);
         setLoading(false);
       })
       .catch(error => {
+        console.error(error);  // Debugging: log error to console
         setError(error);
         setLoading(false);
       });
@@ -28,12 +27,14 @@ function EcoDataPointsPage() {
   if (error) return <div>Error: {error.message}</div>;
 
   return (
-    <div className='edp-container'>
+    <div>
       <h1>Eco Data Points</h1>
       <ul>
-        {data.map(item => (
-          <li key={item.eco_data_point_id}>
-            <strong>{item.eco_data_point_name}</strong>: {item.period}
+        {ecoDataPoints.map(point => (
+          <li key={point.eco_data_point_id}>
+            <Link to={`/eco-data-points/${point.eco_data_point_id}`}>
+              {point.eco_data_point_name} ({point.period})
+            </Link>
           </li>
         ))}
       </ul>
@@ -41,4 +42,4 @@ function EcoDataPointsPage() {
   );
 }
 
-export default EcoDataPointsPage;
+export default EcoDataPoints;
