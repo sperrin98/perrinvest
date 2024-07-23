96867119625d3f5123db692c39a7234e90293dfe
sperrin98 <stanperrin98@gmail.com>
2024-07-23 12:38:08 +0100

    edp frontend

--- frontend/src/components/EcoDataPoints.js
+++ frontend/src/components/EcoDataPoints.js
@@ -1,12 +1,13 @@
 // components/EcoDataPoints.js
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import './EcoDataPoints.css'
+import './EcoDataPoints.css';
 
 function EcoDataPoints() {
   const [ecoDataPoints, setEcoDataPoints] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const [searchTerm, setSearchTerm] = useState(''); // Add this line
 
   useEffect(() => {
     fetch('http://localhost:5000/eco-data-points')
@@ -26,18 +27,45 @@ function EcoDataPoints() {
   if (loading) return <div>Loading...</div>;
   if (error) return <div>Error: {error.message}</div>;
 
+  // Filter ecoDataPoints based on searchTerm
+  const filteredDataPoints = ecoDataPoints.filter(point =>
+    point.eco_data_point_name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
     <div className='edp-container'>
-      <h1>Eco Data Points</h1>
-      <ul>
-        {ecoDataPoints.map(point => (
-          <li key={point.eco_data_point_id}>
-            <Link to={`/eco-data-points/${point.eco_data_point_id}`}>
-              {point.eco_data_point_name} ({point.period})
-            </Link>
-          </li>
-        ))}
-      </ul>
+      <div className='search-container'>
+        <input
+          type='text'
+          placeholder='Search Eco Data Points...'
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          className='search-input'
+        />
+      </div>
+      
+      <table>
+        <thead>
+          <tr>
+            <th className='id-header'></th>
+            <th className='first-header'>Name</th>
+            <th>Period</th>
+          </tr>
+        </thead>
+        <tbody>
+          {filteredDataPoints.map(point => (
+            <tr key={point.eco_data_point_id}>
+              <td>{point.eco_data_point_id}</td>
+              <td>
+                <Link to={`/eco-data-points/${point.eco_data_point_id}`}>
+                  {point.eco_data_point_name}
+                </Link>
+              </td>
+              <td>{point.period}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
