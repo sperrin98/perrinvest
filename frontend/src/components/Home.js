529d6345005970bf211a422f67216af13d697467
sperrin98 <stanperrin98@gmail.com>
2024-07-28 12:02:44 +0100

    buttons and image

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import './Home.css'; // Import the CSS file
 import ChartCarousel from './ChartCarousel';
+import coinImage from '../assets/images/coin.jpg';
 
 const Home = () => {
   const [displayedText, setDisplayedText] = useState('');
@@ -29,7 +30,7 @@ const Home = () => {
   return (
     <div>
       <div className="section section1">
-        <h1 className="home-header">Perrinvest</h1>
+        <h1 className="home-header">{displayedText}</h1>
       </div>
       <div className="section section2">
         <h2 className='section2-header'>Trending Markets</h2>
@@ -37,10 +38,13 @@ const Home = () => {
       </div>
       <div className="section section3">
         <h1 className="home-header">Currencies</h1>
-        <div>
-          <Link to="/currencies"  className='currency-btn'>Currencies</Link>
+        <div className="button-container">
+          <Link to="/currencies" className='currency-btn'>Currencies</Link>
           <Link to="/currencies/divide" className='compare-btn'>Compare Currencies</Link>
-          <Link to="/currencies/crypto" className='compare-btn'>Cryptocurrencies</Link>
+          <Link to="/currencies/crypto" className='crypto-btn'>Cryptocurrencies</Link>
+        </div>
+        <div className="image-container">
+          <img src={coinImage} alt="Coin" /> 
         </div>
       </div>
       <div className="section section4">
