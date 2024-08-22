be1be2981b8285474133fecae7af1e82258556af
sperrin98 <stanperrin98@gmail.com>
2024-08-22 16:47:34 +0100

    Ensure all changes are included

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,13 @@
 from flask import Flask, send_from_directory
 
 def create_app():
-    app = Flask(__name__, static_folder='../frontend/build', template_folder='../frontend/build')
-
+    app = Flask(__name__, static_folder='static', static_url_path='/')
+    
     @app.route('/')
-    def index():
-        return send_from_directory(app.template_folder, 'index.html')
-
     @app.route('/<path:path>')
-    def static_proxy(path):
+    def serve(path=None):
+        if path is None or path == '':
+            path = 'index.html'
         return send_from_directory(app.static_folder, path)
 
     return app
