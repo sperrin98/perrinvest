85810af78ce11bc485b39a88d82baec409b1a451
sperrin98 <stanperrin98@gmail.com>
2024-08-22 17:43:36 +0100

    Fix Gunicorn command for factory function

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,11 @@
+# app/__init__.py
+
 from flask import Flask
-from .main.routes import main  # Import your blueprint from the main subdirectory
 
 def create_app():
     app = Flask(__name__)
-    
-    # Register the blueprint
-    app.register_blueprint(main)
-
+    # Initialize your app, e.g., register blueprints, configure settings
+    @app.route('/')
+    def home():
+        return "Hello, World!"
     return app
