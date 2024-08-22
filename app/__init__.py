58d42ca837d3f552b8e0c57f72633eedfb9d8b65
sperrin98 <stanperrin98@gmail.com>
2024-08-22 17:36:09 +0100

    Deploy to Heroku

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,10 @@
 from flask import Flask
-from .routes import main  # Import your blueprint
+from .main.routes import main  # Import your blueprint from the main subdirectory
 
 def create_app():
     app = Flask(__name__)
     
     # Register the blueprint
     app.register_blueprint(main)
-
-    # Add any other configurations or setup here
-    # For example, app.config.from_object('config.Config')
 
     return app
