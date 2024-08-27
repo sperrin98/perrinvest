85f208b869173d07c4b77c92ca146e6be2ff3555
sperrin98 <stanperrin98@gmail.com>
2024-08-27 20:13:48 +0100

    Update for production deployment

--- run.py
+++ run.py
@@ -1,10 +1,6 @@
-import os
 from app import create_app
-from waitress import serve
 
 app = create_app()
 
-if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
-    serve(app, host='0.0.0.0', port=port)
-
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
