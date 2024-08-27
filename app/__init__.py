36811407693f3f731ebb82caeb229367f71831dc
sperrin98 <stanperrin98@gmail.com>
2024-08-27 18:50:24 +0100

    Initial commit

--- app/__init__.py
+++ app/__init__.py
@@ -1,22 +1,11 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-import os
-
-# Initialize SQLAlchemy
-db = SQLAlchemy()
+from .main.routes import main  # Updated import to reflect the correct path
 
 def create_app():
     app = Flask(__name__)
-    
-    # Configure the app
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'mysql+mysqlconnector://root:@localhost:3306/perrinvest')
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # To suppress warnings
-    
-    # Initialize SQLAlchemy with the app
-    db.init_app(app)
-    
-    # Register blueprints
-    from .routes import main  # Import routes from within the app package
-    app.register_blueprint(main)
-    
+
+    app.register_blueprint(main)  # Register the main blueprint
+
+    # Other app setup here (configurations, extensions, etc.)
+
     return app
