58c8da543c800f8c618f5137b4f6094391ce0c13
sperrin98 <stanperrin98@gmail.com>
2024-08-27 20:14:49 +0100

    Update for production deployment

--- config.py
+++ config.py
@@ -1,7 +1,4 @@
 class Config:
-    SQLALCHEMY_DATABASE_URI = (
-        'mysql+mysqlconnector://k3vplcl9qdlqp9tm:i22fwidifwtddran@'
-        'q57yawiwmnaw13d2.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/xsgfymqt315ntykm'
-    )
+    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:Royals106#@localhost/perrinvest'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = 'b6e3f6e8039e81a0c4e3e3e0e6b1a2748f5e9184b918b7e4'  # Replace with your generated key
+    SECRET_KEY = 'my_secret_key'
