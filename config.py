967e24bc82f780fb34ff9fb502e8e9541c3327d7
sperrin98 <stanperrin98@gmail.com>
2024-08-27 12:11:45 +0100

    Update configuration to use JawsDB URL

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'mysql+mysqlconnector://root:Royals106#@localhost/perrinvest')
+    SQLALCHEMY_DATABASE_URI = os.getenv('JAWSDB_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.getenv('SECRET_KEY', 'my_secret_key')
