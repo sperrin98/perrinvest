7081fb23202a447405fc4288d5b8b73e3874a279
sperrin98 <stanperrin98@gmail.com>
2024-08-27 22:51:22 +0100

    Update to use wsgi for deployment

--- config.py
+++ config.py
@@ -3,10 +3,10 @@ import os
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret_key')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'mysql+mysqlconnector://root:Royals106#@localhost/perrinvest')
 
 class ProductionConfig(Config):
     DEBUG = False
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
 
 class DevelopmentConfig(Config):
     DEBUG = True
