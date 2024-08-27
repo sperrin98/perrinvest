53e3c55fa122162b30f2b47609ae75c763f8f6eb
sperrin98 <stanperrin98@gmail.com>
2024-08-27 12:25:56 +0100

    Prepare for Heroku deployment

--- app/__init__.py
+++ app/__init__.py
@@ -1,19 +1,15 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_cors import CORS
+from config import Config
 
 db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_object('config.Config')
-
+    app.config.from_object(Config)
     db.init_app(app)
 
-    # Enable CORS for all routes
-    CORS(app)
-
-    from app.main import main as main_blueprint
-    app.register_blueprint(main_blueprint)
+    from .routes import main
+    app.register_blueprint(main)
 
     return app
