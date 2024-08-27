8691829af8fe5389e05bfb6ddd4ac873f227eba6
sperrin98 <stanperrin98@gmail.com>
2024-08-27 11:34:59 +0100

    'change'

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -1,8 +1,5 @@
-from flask import Flask
+from flask import Blueprint
 
-def create_app():
-    app = Flask(__name__)
-    # Additional configuration and initialization
-    from app.main.routes import main
-    app.register_blueprint(main)
-    return app
+main = Blueprint('main', __name__)
+
+from app.main import routes
--- wsgi.py
+++ wsgi.py
@@ -1,3 +1,4 @@
-from app import create_app
+from app import app  # or from your app's main module
 
-app = create_app()
+if __name__ == "__main__":
+    app.run()
