935a11d1083ca8f0e2e8448b3303b77d44d1be40
sperrin98 <stanperrin98@gmail.com>
2024-08-27 20:31:41 +0100

    ready for deployment

--- config.py
+++ config.py
@@ -1,4 +1,9 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()  # Load environment variables from a .env file if you have one
+
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:Royals106#@localhost/perrinvest'
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = 'my_secret_key'
+    SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret_key')
