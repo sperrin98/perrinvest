b551ca27a589e7afd0238f259de5896ea1838785
sperrin98 <stanperrin98@gmail.com>
2024-08-27 20:10:21 +0100

    changes

--- config.py
+++ config.py
@@ -1,3 +1,7 @@
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://k3vplcl9qdlqp9tm:pvpfenwt8cdz9hsd@q57yawiwmnaw13d2.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/xsgfymqt315ntykm'
+    SQLALCHEMY_DATABASE_URI = (
+        'mysql+mysqlconnector://k3vplcl9qdlqp9tm:i22fwidifwtddran@'
+        'q57yawiwmnaw13d2.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/xsgfymqt315ntykm'
+    )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = 'b6e3f6e8039e81a0c4e3e3e0e6b1a2748f5e9184b918b7e4'  # Replace with your generated key
--- run.py
+++ run.py
@@ -1,22 +1,10 @@
-import sys
+import os
 from app import create_app
-from flask_migrate import upgrade
+from waitress import serve
 
 app = create_app()
 
-def main():
-    if len(sys.argv) > 1:
-        if sys.argv[1] == 'db' and len(sys.argv) > 2:
-            if sys.argv[2] == 'upgrade':
-                with app.app_context():
-                    upgrade()
-            else:
-                print("Usage: python run.py db upgrade")
-        else:
-            print("Usage: python run.py db upgrade")
-    else:
-        # Run the app in production mode
-        app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
-
 if __name__ == "__main__":
-    main()
+    port = int(os.environ.get('PORT', 5000))
+    serve(app, host='0.0.0.0', port=port)
+
