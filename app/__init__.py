8f1f9eaadcc5e06edfc412fac8a98722ab6d0274
sperrin98 <stanperrin98@gmail.com>
2024-08-27 18:28:26 +0100

    Initial commit

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,18 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import os
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'mysql+mysqlconnector://root:@localhost:3306/perrinvest')
-db = SQLAlchemy(app)
+# Initialize SQLAlchemy
+db = SQLAlchemy()
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'mysql+mysqlconnector://root:@localhost:3306/perrinvest')
+    
+    # Initialize the app with SQLAlchemy
+    db.init_app(app)
+    
+    # Register blueprints
+    from .routes import main
+    app.register_blueprint(main)
+    
+    return app
