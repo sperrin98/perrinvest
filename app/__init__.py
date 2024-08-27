4d61bd61ba6712cd70b066195d98d37af4e72bfb
sperrin98 <stanperrin98@gmail.com>
2024-08-27 20:10:21 +0100

    changes

--- app/__init__.py
+++ app/__init__.py
@@ -1,25 +1,19 @@
-import os  # Import the os module at the top
-
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
+from flask_cors import CORS
 
-# Initialize the database and migration objects
 db = SQLAlchemy()
-migrate = Migrate()
 
 def create_app():
     app = Flask(__name__)
-
-    # Database configuration from Heroku environment variables
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config.from_object('config.Config')
 
     db.init_app(app)
-    migrate.init_app(app, db)
 
-    # Register your blueprints
-    from .main.routes import main
-    app.register_blueprint(main)
+    # Enable CORS for all routes
+    CORS(app)
+
+    from app.main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
 
     return app
