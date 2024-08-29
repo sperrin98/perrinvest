ce54f6feb9234066f4f1d884475ec2430728737e
sperrin98 <stanperrin98@gmail.com>
2024-08-29 12:02:24 +0100

    Prepare for deployment to Heroku

--- config.py
+++ config.py
@@ -1,17 +1,4 @@
 import os
 
 class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret_key')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-class ProductionConfig(Config):
-    DEBUG = False
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
-
-class DevelopmentConfig(Config):
-    DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:Royals106#@localhost/perrinvest'
-
-class TestingConfig(Config):
-    TESTING = True
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'mysql+mysqlconnector://k3vplcl9qdlqp9tm:i22fwidifwtddran@q57yawiwmnaw13d2.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/xsgfymqt315ntykm')
--- run.py
+++ run.py
@@ -1,15 +1,4 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
-
-app = Flask(__name__)
-app.config.from_object('config.DevelopmentConfig')  # Ensure this points to the correct config class
-
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
-
-# Import your models after initializing db
-from app import models  # Adjust the import path if needed
+from app import app
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
