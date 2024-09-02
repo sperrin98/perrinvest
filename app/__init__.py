6385cc31f3c24beeb58b57c10319a11fd18c25d4
sperrin98 <stanperrin98@gmail.com>
2024-09-02 13:35:17 +0100

    Fix database configuration

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,19 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+db = SQLAlchemy()
+migrate = Migrate()
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_object('app.config.Config')  # Updated path
-
+    app.config.from_object('app.config.Config')  # Correct path to config
+    
+    db.init_app(app)
+    migrate.init_app(app, db)
+    
+    # Register blueprints if any
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
-
+    
     return app
--- app/config.py
+++ app/config.py
@@ -1,5 +1,6 @@
+# app/config.py
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
-    SECRET_KEY = os.getenv('SECRET_KEY', 'Royals106#') 
+    SQLALCHEMY_DATABASE_URI = os.getenv('CLEARDB_URL', 'mysql://bef8d401dbcad8:8dd3aadb@us-cluster-east-01.k8s.cleardb.net/heroku_cdb808eab650119?reconnect=true')
+    SECRET_KEY = os.getenv('SECRET_KEY', 'Royals106#')
