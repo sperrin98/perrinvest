6317065041ba5f57b083037b84cf57e1f8a370b4
sperrin98 <stanperrin98@gmail.com>
2024-09-02 13:11:05 +0100

    Describe your changes

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('CLEARDB_DATABASE_URL')  # Use ClearDB URL
-    SECRET_KEY = os.getenv('SECRET_KEY', 'your_secret_key') 
+    SQLALCHEMY_DATABASE_URI = os.getenv('CLEARDB_DATABASE_URL')
+    SECRET_KEY = os.getenv('SECRET_KEY', 'Royals106#')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
