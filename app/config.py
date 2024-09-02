4282cb91d72170288bd63d0878192cc2f9c579d7
sperrin98 <stanperrin98@gmail.com>
2024-09-02 13:07:48 +0100

    Describe your changes

--- app/config.py
+++
@@ -1,5 +0,0 @@
-import os
-
-class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
-    SECRET_KEY = os.getenv('SECRET_KEY', 'Royals106#')  # Replace 'default_secret_key' with a more secure key or manage it via environment variable.
