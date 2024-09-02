d15b5312701ad6743e1fed6c0f903cfe2e84de7b
sperrin98 <stanperrin98@gmail.com>
2024-09-02 13:14:02 +0100

    Describe your changes

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,8 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
-
-db = SQLAlchemy()
-migrate = Migrate()
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_object('app.config.Config')
-
-    db.init_app(app)
-    migrate.init_app(app, db)
+    app.config.from_object('app.config.Config')  # Updated path
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
