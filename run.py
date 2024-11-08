879692212443838f679827049fcb64f3afb949cf
sperrin98 <stanperrin98@gmail.com>
2024-11-08 16:23:59 +0000

    env

--- run.py
+++ run.py
@@ -1,6 +1,8 @@
-from app import create_app
+from flask import Flask
 
-app = create_app()
+# Create the Flask app
+app = Flask(__name__)
 
+# Ensure the Flask app is accessible from any network by binding it to 0.0.0.0
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000)
