96813a3b7aee9f79db5e880f5a6cba07d5cf6962
sperrin98 <stanperrin98@gmail.com>
2024-11-28 16:29:09 +0000

    correlation-frontend

--- run.py
+++ run.py
@@ -4,5 +4,5 @@ import os
 app = create_app()
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # Default to 5000 if PORT isn't set
+    port = int(os.environ.get("PORT", 5000))  
     app.run(host="0.0.0.0", port=port)
