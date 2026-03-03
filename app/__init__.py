b9f66c72babffdbf940edd824a3905f9bb7bee6f
sperrin98 <stanperrin98@gmail.com>
2026-03-03 11:26:34 +0000

    Remove debug logging of secrets

--- app/__init__.py
+++ app/__init__.py
@@ -13,7 +13,6 @@ def create_app():
     # Debug log to ensure .env values are loaded
     print("Environment Variables Loaded:")
     print(f"DB_USER: {os.getenv('DB_USER')}")
-    print(f"DB_PASSWORD: {os.getenv('DB_PASSWORD')}")
     print(f"DB_HOST: {os.getenv('DB_HOST')}")
     print(f"DB_NAME: {os.getenv('DB_NAME')}")
 
