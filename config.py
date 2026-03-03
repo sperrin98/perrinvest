814af26a2786263911ca1e5fb10677a53f3fdbbe
sperrin98 <stanperrin98@gmail.com>
2026-03-03 11:28:19 +0000

    Remove hardcoded DB credentials and use environment variables

--- config.py
+++ config.py
@@ -1,15 +1,8 @@
 import os
 
 class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret_key')  # Default value
+    SECRET_KEY = os.environ.get("SECRET_KEY")
 
-    # Dynamically construct SQLAlchemy URI based on environment variables
-    SQLALCHEMY_DATABASE_URI = os.getenv(
-        'SQLALCHEMY_DATABASE_URI',  # Primary: Look for SQLALCHEMY_DATABASE_URI
-        f"mysql+mysqlconnector://{os.getenv('DB_USER', 'root')}:"
-        f"{os.getenv('DB_PASSWORD', '')}@"
-        f"{os.getenv('DB_HOST', '35.232.119.108')}/"
-        f"{os.getenv('DB_NAME', 'perrinvest')}"  # Secondary: Construct from other env vars
-    )
+    SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI")
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
